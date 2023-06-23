abe10fb0054e8502b7d6206ffae02f8840d7d185
ustyuzhanin <anton.ustyuzhanin@me.com>
2023-06-23 22:48:34 +0300

    p

--- valsrevenge/Player.swift
+++ valsrevenge/Player.swift
@@ -14,9 +14,15 @@ enum Direction: String {
     case right
     case up
     case down
+    case topLeft
+    case topRight
+    case bottomLeft
+    case bottomRight
 }
 
 class Player: SKSpriteNode {
+    
+    private var currentDirection = Direction.stop
     
     func move(_ direction: Direction) {
         print("move player: \(direction.rawValue)")
@@ -33,10 +39,55 @@ class Player: SKSpriteNode {
             self.physicsBody?.velocity = CGVector(dx: 100, dy: 0)
         case .stop:
             stop()
+        case .topLeft:
+            self.physicsBody?.velocity = CGVector(dx: -100, dy: 100)
+        case .topRight:
+            self.physicsBody?.velocity = CGVector(dx: 100, dy: 100)
+        case .bottomLeft:
+            self.physicsBody?.velocity = CGVector(dx: -100, dy: -100)
+        case .bottomRight:
+            self.physicsBody?.velocity = CGVector(dx: 100, dy: -100)
         }
+        
+        if direction != .stop {
+            currentDirection = direction
+        }
+        
     }
     
     func stop() {
         self.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+    }
+    
+    func attack() {
+        let projectile = SKSpriteNode(imageNamed: "knife")
+        projectile.position = CGPoint(x: 0.0, y: 0.0)
+        addChild(projectile)
+        
+        var throwDirection = CGVector(dx: 0, dy: 0)
+        
+        switch currentDirection {
+        case .up:
+            throwDirection = CGVector(dx: 0, dy: 300)
+        case .down:
+            throwDirection = CGVector(dx: 0, dy: -300)
+        case .left:
+            throwDirection = CGVector(dx: -300, dy: 0)
+        case .right, .stop:
+            throwDirection = CGVector(dx: 300, dy: 0)
+        case .topLeft:
+            throwDirection = CGVector(dx: -300, dy: 300)
+        case .topRight:
+            throwDirection = CGVector(dx: 300, dy: 300)
+        case .bottomLeft:
+            throwDirection = CGVector(dx: -300, dy: -300)
+        case .bottomRight:
+            throwDirection = CGVector(dx: 300, dy: -300)
+        }
+        
+        let throwProjectile = SKAction.move(by: throwDirection, duration: 0.25)
+        projectile.run(throwProjectile, completion:  {
+            projectile.removeFromParent()
+        })
     }
 }
